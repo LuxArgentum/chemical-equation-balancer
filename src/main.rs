167e2b92440c10a4b3f426b7afd57a2e6e6d2a3d
MatthewSong <matthewsong18@gmail.com>
2024-04-19 13:33:17 -0700

    Add equation input from command-line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter a chemical equation to balance!");
+    
+    let test_string = "2N_2H_4 + N_2O_4 -> 3N_2 + 4H_2O";
+    
+    let mut input_string = String::new();
+    stdin()
+        .read_line(&mut input_string)
+        .expect("That's not a valid string");
+    input_string.pop();
+    
+    println!("Input String: {}", input_string);
+    println!("Test String: {}", test_string);
 }
